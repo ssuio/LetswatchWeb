0b51c0dfc625a2a544358d6e62a90f0881cff063
Cyruss <Cyruss@Cyrus>
2016-05-25 20:35:22 +0800

    commit 0525
    Login datepicker register home

---
+++ web/js/datepicker.js
@@ -0,0 +1,9 @@
+$(function() {
+    $( "#datepicker" ).datepicker( 
+            {dateFormat: "yy-mm-dd",
+             changeMonth: true,
+             changeYear: true,
+             regional:"zh-TW"
+            }
+            );
+  });
